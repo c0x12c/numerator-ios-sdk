67838d555976728c75bb382bedefb2860e84a704
Tony DangK <tuong.dang@c0x12c.com>
2024-03-05 16:34:17 +0700

    Update Swift Package Manager files and ignore unnecessary files

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "Intercom",
+            name: "NumeratorSDK",
             url: "https://github.com/c0x12c/numerator-ios-sdk/releases/download/1.0.0/NumeratorSDK.xcframework.zip",
             checksum: "b4f0d88f299a35fa1d68d69cf82783568575e741113ad9993123cdb2793d0cda"
         )
