546b5374af1049c325421d746a641b45d73358ba
Tony DangK <tuong.dang@c0x12c.com>
2024-03-05 16:19:08 +0700

    Add file Package.swift is support SPM

--- Examples/README.md
+++ Examples/README.md
@@ -2,5 +2,5 @@ The example apps provide a basic demo of Numerator iOS SDK.
 
 ## SwiftUI
 1. Go to **/Sample-SwiftUI** and open **Sample-SwiftUI.xcodeproj**
-2. Set your iOS **API key** at the top of `SceneDelegate.swift`
+2. Set your iOS **API key** at the top of `ContentView.swift`
 3. Build and Run 🙌
---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NumeratorSDK",
+    products: [
+        .library(
+            name: "NumeratorSDK",
+            targets: ["NumeratorSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Intercom",
+            url: "https://github.com/c0x12c/numerator-ios-sdk/releases/download/1.0.0/NumeratorSDK.xcframework.zip",
+            checksum: "b4f0d88f299a35fa1d68d69cf82783568575e741113ad9993123cdb2793d0cda"
+        )
+    ]
+)
