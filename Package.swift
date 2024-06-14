c5aea0ab6dabed4f01535ccb04b05ec68f590d85
Tony DangK <tuong.dang@c0x12c.com>
2024-06-14 11:24:14 +0700

    Release version 0.0.6

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NumeratorSDK",
-            url: "https://github.com/numerator-io/sdk-ios/releases/download/0.0.5/NumeratorSDK.xcframework.zip",
+            url: "https://github.com/numerator-io/sdk-ios/releases/download/0.0.6/NumeratorSDK.xcframework.zip",
             checksum: "ab1fb80d0aff2ab97d7012734369b8ee6ecf053ba379f5125d9de9fb9d248690"
         )
     ]
